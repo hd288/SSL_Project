dd67241505a1de3c036c4b9ce196fa85887dddbe
Tominq <jay.17891@gmail.com>
2023-12-15 13:47:54 +0700

    chore!: project structure

---
+++ SSL_BE/src/test/java/com/vn/ssl_be/SslBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.ssl_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
