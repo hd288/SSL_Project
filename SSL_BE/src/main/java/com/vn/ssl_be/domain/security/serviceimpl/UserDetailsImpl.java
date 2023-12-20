6ea87ad4b1c174c1f65e777a80dd16db53842da5
Đức Vũ <ducvu2509@gmail.com>
2023-12-20 09:46:39 +0700

    Duc commit security 20/12/2023

---
+++ SSL_BE/src/main/java/com/vn/ssl_be/domain/security/serviceimpl/UserDetailsImpl.java
@@ -0,0 +1,73 @@
+package com.vn.ssl_be.domain.security.serviceimpl;
+
+
+import com.vn.ssl_be.domain.security.model.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private String email;
+    private String password;
+    private List<GrantedAuthority> authorities;
+    private boolean enable;
+
+    public static UserDetailsImpl build(User user){
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+                .collect(Collectors.toList());
+
+
+        return new UserDetailsImpl(
+                user.getEmail(),
+                user.getPassword(),
+                authorities,
+                user.isEnable());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enable;
+    }
+}
