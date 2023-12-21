ea9853315201c4c2b6fe37710e764a7d34f0cce7
Tominq <110572536+Tominq@users.noreply.github.com>
2023-12-21 11:30:02 +0700

    chore!: fix conflict

--- SSL_BE/src/main/java/com/vn/ssl_be/domain/student/model/StudentQuestionId.java
+++ SSL_BE/src/main/java/com/vn/ssl_be/domain/student/model/StudentQuestionId.java
@@ -2,11 +2,12 @@ package com.vn.ssl_be.domain.student.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 @Embeddable
+@Setter
 public class StudentQuestionId implements Serializable {
     @Column(name = "user_id")
     private String userId;
